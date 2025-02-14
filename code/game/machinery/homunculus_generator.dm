8ebcad206dd174c59d4187d26ec8faed0ee7ec43
VileBeggar <edmir995@gmail.com>
2025-02-14 17:54:22 +0100

    more stuff

--- code/game/machinery/homunculus_generator.dm
+++ code/game/machinery/homunculus_generator.dm
@@ -1,3 +1,7 @@
+#define CELL_GROWTH_GOAL 5 MINUTES
+#define STATUS_ON 1
+#define STATUS_OFF 0
+
 /obj/structure/machinery/homunculus_generator
 	name = "homunculus generator"
 	desc = "An eerie machine that is used for generating crude and grotesque imitations of human beings for experimental purposes."
@@ -8,8 +12,10 @@
 	layer = BELOW_OBJ_LAYER
 
 	var/obj/item/reagent_container/glass/beaker
-	var/occupied = FALSE
+	var/mob/living/carbon/occupant
+	var/obj/item/cell_sample/sample
 	var/growth_rate = 0
+	var/status = STATUS_OFF
 
 /obj/structure/machinery/homunculus_generator/attack_hand(mob/user)
 	if(!skillcheck(user, SKILL_RESEARCH, SKILL_RESEARCH_TRAINED))
@@ -19,17 +25,70 @@
 	tgui_interact(user)
 	return ..()
 
-/obj/structure/machinery/homunculus_generator/attackby(obj/item/reagent_container/attacking_object, mob/user)
-	if(istype(attacking_object, /obj/item/reagent_container/glass) && user.drop_inv_item_to_loc(attacking_object, src))
+/obj/structure/machinery/homunculus_generator/ui_act(action, list/params, datum/tgui/ui, datum/ui_state/state)
+	. = ..()
+	if(.)
+		return
+	switch(action)
+		if("eject_beaker")
+			eject_item(usr, beaker)
+			beaker = null
+			. = TRUE
+		if("eject_sample")
+			eject_item(usr, sample)
+			sample = null
+			. = TRUE
+		if("toggle_cycle")
+			toggle_cycle()
+			. = TRUE
+
+/obj/structure/machinery/homunculus_generator/proc/toggle_cycle()
+	if(!sample)
+		return
+	if(status == STATUS_OFF)
+		status = STATUS_ON
+		START_PROCESSING(SSobj, src)
+	else
+		status = STATUS_OFF
+		STOP_PROCESSING(SSobj, src)
+
+/obj/structure/machinery/homunculus_generator/process()
+	if(beaker.reagents.total_volume <= 0)
+		STOP_PROCESSING(SSobj, src)
+		status = STATUS_OFF
+
+	sample.growth += 1 SECONDS * growth_rate / 2
+	beaker.reagents.remove_all_type(/datum/reagent, 2.5, 0, 1)
+
+/obj/structure/machinery/homunculus_generator/proc/eject_item(mob/living/user, obj/item/item_to_eject)
+	item_to_eject.forceMove(loc)
+	if(user && Adjacent(user))
+		user.put_in_hands(item_to_eject)
+	update_icon()
+	SStgui.update_uis(src)
+
+/obj/structure/machinery/homunculus_generator/attackby(obj/item/reagent_container/item_to_insert, mob/user)
+	if(!user.drop_inv_item_to_loc(item_to_insert, src))
+		return
+
+	if(istype(item_to_insert, /obj/item/cell_sample))
+		var/obj/item/old_sample = sample
+		sample = item_to_insert
+		swap_item(item_to_insert, user, old_sample)
+
+	else if(istype(item_to_insert, /obj/item/reagent_container/glass/beaker))
 		var/obj/item/old_beaker = beaker
-		beaker = attacking_object
-		if(old_beaker)
-			to_chat(user, SPAN_NOTICE("You swap out [old_beaker] for [attacking_object]."))
-			user.put_in_hands(old_beaker)
-		else
-			to_chat(user, SPAN_NOTICE("You set [attacking_object] on the machine."))
+		beaker = item_to_insert
+		swap_item(item_to_insert, user, old_beaker)
 		calculate_growth_rate()
-		SStgui.update_uis(src)
+
+/obj/structure/machinery/homunculus_generator/proc/swap_item(obj/item/item_to_insert, mob/user, obj/item/old_item)
+	if(old_item)
+		to_chat(user, SPAN_NOTICE("You swap out [old_item] for [item_to_insert]."))
+		user.put_in_hands(old_item)
+	else
+		to_chat(user, SPAN_NOTICE("You place [item_to_insert] into [src]."))
+	SStgui.update_uis(src)
 
 /obj/structure/machinery/homunculus_generator/tgui_interact(mob/user, datum/tgui/ui)
 	ui = SStgui.try_update_ui(user, src, ui)
@@ -43,6 +102,9 @@
 		data["beaker"] = beaker.name
 		data["beaker_vol_max"] = beaker.volume
 		data["beaker_vol_cur"] = beaker.reagents.total_volume
+	if(sample)
+		data["sample"] = sample.name
+		data["sample_growth"] = sample.growth
 	data["growth_rate"] = growth_rate
 	data["growth_time"] = calculate_growth_time()
 	return data
@@ -73,4 +135,18 @@
 	if(!growth_rate)
 		return "N/A"
 
-	return duration2text_sec(2400 / growth_rate)
+	return duration2text_sec((CELL_GROWTH_GOAL - sample.growth) / (growth_rate / 2))
+
+/// CELL SAMPLE ITEM
+///------------------
+/obj/item/cell_sample
+	name = "cell sample"
+	desc = "An unassuming tissue sample."
+	icon = 'icons/obj/items/seeds.dmi'
+	icon_state = "seed"
+	var/growth = 0
+
+/obj/item/cell_sample/get_examine_text(mob/user)
+	. = ..()
+	if(skillcheck(user, SKILL_RESEARCH, SKILL_RESEARCH_TRAINED))
+		. += SPAN_NOTICE("You could use this tissue sample to generate a homunculus with the cell generator.")
--- tgui/packages/tgui/interfaces/HomunculusGenerator.tsx
+++ tgui/packages/tgui/interfaces/HomunculusGenerator.tsx
@@ -7,6 +7,7 @@ import {
   Icon,
   ProgressBar,
   Section,
+  Tooltip,
 } from '../components';
 import { Window } from '../layouts';
 
@@ -16,6 +17,7 @@ type Data = {
   beaker_vol_cur: number;
   growth_rate: number;
   growth_time: string;
+  sample: string;
 };
 const { act, data } = useBackend<Data>();
 
@@ -34,18 +36,38 @@ const ItemPanel = (props) => {
   return (
     <Section title="INFORMATION">
       <Flex direction="row" wrap="wrap">
+        <Flex.Item>
+          <Icon name="circle-nodes" />
+        </Flex.Item>
+        <Divider hidden vertical />
         <Flex.Item grow={0.9}>
-          <Box>Homunculus sample: cell sample</Box>
+          <Box>HOMUNCULUS SAMPLE: {data.sample ? `${data.sample}` : 'N/A'}</Box>
         </Flex.Item>
         <Flex.Item>
-          <Button fluid>EJECT SAMPLE</Button>
+          <Button
+            fluid
+            disabled={data.sample ? false : true}
+            onClick={() => act('eject_sample')}
+          >
+            EJECT SAMPLE
+          </Button>
         </Flex.Item>
         <FlexLineBreak />
+        <Flex.Item>
+          <Icon name="flask" />
+        </Flex.Item>
+        <Divider hidden vertical />
         <Flex.Item grow={0.9}>
-          <Box>Nutrient beaker: {data.beaker ? `${data.beaker}` : 'N/A'}</Box>
+          <Box>NUTRIENT BEAKER: {data.beaker ? `${data.beaker}` : 'N/A'}</Box>
         </Flex.Item>
         <Flex.Item>
-          <Button fluid>EJECT BEAKER</Button>
+          <Button
+            fluid
+            disabled={data.beaker ? false : true}
+            onClick={() => act('eject_beaker')}
+          >
+            EJECT BEAKER
+          </Button>
         </Flex.Item>
       </Flex>
     </Section>
@@ -53,19 +75,9 @@ const ItemPanel = (props) => {
 };
 
 const StatusPanel = (props) => {
+  const fluidLevel = data.beaker_vol_cur / data.beaker_vol_max;
   return (
-    <Flex direction="row" wrap="wrap" justify="space-between">
-      <Flex.Item>
-        <Section title="CONTROL PANEL">
-          <Flex.Item>
-            <Button>START GENERATIVE CYCLE</Button>
-          </Flex.Item>
-          <Flex.Item>
-            <Button>EJECT OCCUPANT</Button>
-          </Flex.Item>
-        </Section>
-      </Flex.Item>
-
+    <Flex direction="row" justify="space-between">
       <Flex.Item width="50%">
         <Section title="CYCLE INFORMATION">
           <Flex justify="space-around">
@@ -73,22 +85,62 @@ const StatusPanel = (props) => {
               <Box textAlign="center">
                 <Icon name="temperature-full" /> FLUID LEVEL:
               </Box>
-              <ProgressBar value={0.5}>50/50</ProgressBar>
+              <ProgressBar
+                value={fluidLevel}
+                ranges={{
+                  good: [0.5, Infinity],
+                  average: [0.25, 0.5],
+                  bad: [-Infinity, 0.25],
+                }}
+              >
+                {data.beaker ? data.beaker_vol_cur : 0}u /{' '}
+                {data.beaker ? data.beaker_vol_max : 0}u
+              </ProgressBar>
             </Flex.Item>
             <Divider hidden vertical />
             <Flex.Item grow={0.5}>
               <Box textAlign="center">
                 <Icon name="person" /> MATURITY:
               </Box>
+
               <ProgressBar value={0.5}>50/50</ProgressBar>
             </Flex.Item>
           </Flex>
           <Divider />
+          <Flex justify="space-between">
+            <Flex.Item>
+              <Box>
+                GROWTH RATE: <GrowthRateText /> ({data.growth_rate}){' '}
+                <Divider hidden /> CYCLE FINISH IN: {data.growth_time}
+              </Box>
+            </Flex.Item>
+            <Tooltip
+              position="bottom"
+              content="NOTE: The following chemical properties increase growth rate: HEMOGENIC, NUTRITIOUS, HYPERGENETIC."
+            >
+              <Flex.Item align="center" grow={0.5}>
+                <Box>
+                  <Icon size={2} name="circle-info" />
+                </Box>
+              </Flex.Item>
+            </Tooltip>
+          </Flex>
+        </Section>
+      </Flex.Item>
+
+      <Flex.Item>
+        <Section title="CONTROL PANEL">
           <Flex.Item>
-            <Box inline>
-              GROWTH RATE: <GrowthRateText /> {data.growth_rate}{' '}
-              <Divider hidden /> CYCLE FINISH IN: ({data.growth_time})
-            </Box>
+            <Button
+              fluid
+              disabled={data.sample ? false : true}
+              onClick={() => act('toggle_cycle')}
+            >
+              START GENERATIVE CYCLE
+            </Button>
+          </Flex.Item>
+          <Flex.Item>
+            <Button fluid>EJECT OCCUPANT</Button>
           </Flex.Item>
         </Section>
       </Flex.Item>
